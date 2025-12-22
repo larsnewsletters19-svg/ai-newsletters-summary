33b97e2c96d5b8c69bf037d1d11d1bee0266b7aa
larsnewsletters19-svg <lars.newsletters19@gmail.com>
2025-12-22 10:25:09 +0100

    Add files via upload

---
+++ utils/logger.py
@@ -0,0 +1,21 @@
+"""Logger setup"""
+
+import logging
+import sys
+
+def setup_logger():
+    """Setup logger för applikationen"""
+    logger = logging.getLogger('newsletter_summary')
+    logger.setLevel(logging.INFO)
+    
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+    
+    logger.addHandler(handler)
+    
+    return logger
